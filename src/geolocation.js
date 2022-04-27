1b22b33f137b68ff57cc8fccc5a3dc15427c29f4
Nimeavles <prlf505@gmail.com>
2022-04-27 15:50:07 +0200

    Implementing the geolocation API

---
+++ src/geolocation.js
@@ -0,0 +1,8 @@
+let lat,lon;
+let geo = navigator.geolocation.getCurrentPosition((data) => {
+    lat = data.coords.latitude;
+    lon = data.coords.longitude;
+
+    return let, lon
+});
+
--- src/getWheather.js
+++ src/getWheather.js
@@ -1,8 +1,30 @@
 const url = "https://api.allorigins.win/get?url=https://www.metaweather.com/api/location/search/?query=";
 const url_woeid = "https://api.allorigins.win/get?url=https://www.metaweather.com/api/location/";
 let id = [];
-let daysPlace = document.getElementById(days);
 let icon;
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+class representData {
+  constructor() {
+    this.place = document.getElementById('days');
+  }
+
+  create(date, icon, max_temp, min_temp) {
+    let div = document.createElement('div');
+    div.setAttribute('class', 'each_day')
+    div.innerHTML = (`
+    <p>${date}</p>
+    <img src="${icon}" class="img">
+    <div class="min_max">
+      <p>${max_temp}º</p>
+      <p>${min_temp}º</p>
+    </div>
+    `);
+    this.place.appendChild(div);
+  }
+}
+
+const RepresentData = new representData();
 
 const getWoeid = (place) => new Promise((resolve, reject) => {
   request({ uri: `${url}${place}` }, (err, res, body) => {
@@ -37,54 +59,51 @@ function wheather(woeid) {
       new Error(err)
     } else {
       let data = JSON.parse(body);
-      let contents = data['contents'];
-      let jsonData = JSON.parse(contents);
-      let days = jsonData["consolidated_weather"];
-      days.forEach(element => {
+      data = data['contents'];
+      data = JSON.parse(data);
+      data = data["consolidated_weather"];
+      data.forEach(element => {
         switch (element.weather_state_abbr) {
           case 'sn':
-            icon = 'assets/Snow.svg';
+            icon = '../assets/Snow.svg';
             break;
           case 'sl':
-            icon = 'assets/Sleet.svg';
+            icon = '../assets/Sleet.svg';
             break;
           case 'h':
-            icon = 'assets/Hail.svg';
+            icon = '../assets/Hail.svg';
             break;
           case 't':
-            icon = 'assets/ThunderStorm.svg';
+            icon = '../assets/ThunderStorm.svg';
             break;
           case 'hr':
-            icon = 'assets/HeavyRain.svg';
+            icon = '../assets/HeavyRain.svg';
             break;
           case 'lr':
-            icon = 'assets/LightRain.svg';
+            icon = '../assets/LightRain.svg';
             break;
           case 's':
-            icon = 'assets/Showers.svg';
+            icon = '../assets/Showers.svg';
             break;
           case 'hc':
-            icon = 'assets/HeavyCloud.svg';
+            icon = '../assets/HeavyCloud.svg';
             break;
           case 'lc':
-            icon = 'assets/LightCloud.svg';
+            icon = '../assets/LightCloud.svg';
             break;
           case 'c':
-            icon = 'assets/Clear.svg';
+            icon = '../assets/Clear.svg';
             break;
         }
-        document.getElementById(days).innerHTML =
-        `<div>
-          <p>${element.applicable_date}</p>
-          <img src="${icon}>
-          <div>
-            <p>${element.max_temp}</p>
-            <p>${element.min_temp}</p>
-          </div>
-        </div>`
+        let date = new Date(element.applicable_date);
+
+        date = date.toDateString('es-ES', options)
+        RepresentData.create(date, icon, Math.round( element.max_temp), Math.round(element.min_temp));
       });
+      
     }
   })
+
 }
 
 wheather(44418);
